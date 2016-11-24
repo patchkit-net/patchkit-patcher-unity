04d56f4f70395987624fd09ae7e1b48a5f2d92b3
Tomasz Jaworski <tomasz.jaworski98@gmail.com>
2016-11-24 19:33:24 +0100

    Add exception logging functions for debug logger

--- src/Assets/Scripts/Log/IDebugLoggerExtensions.cs
+++ src/Assets/Scripts/Log/IDebugLoggerExtensions.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace PatchKit.Unity.Patcher.Log
 {
@@ -10,9 +11,31 @@ namespace PatchKit.Unity.Patcher.Log
             return obj.GetType().Name;
         }
 
+        private static string FormatExceptionLog(Exception exception)
+        {
+            return string.Format("{0}\n\nStack trace:\n{1}", exception.Message, exception.StackTrace);
+        }
+
         public static void Log(this IDebugLogger @this, object message)
         {
-            Debug.Log(string.Format("[{0}] {1}", GetTypeName(@this), message));
+            Debug.LogFormat("[{0}] {1}", GetTypeName(@this), message);
+        }
+
+        public static void LogException(this IDebugLogger @this, Exception exception)
+        {
+            Debug.LogErrorFormat("[{0}] Exception: {1}", GetTypeName(@this), FormatExceptionLog(exception));
+            int innerExceptionCounter = 1;
+            var innerException = exception.InnerException;
+            while (innerException != null)
+            {
+                Debug.LogErrorFormat("[{0}] Inner Exception {1}: {2}", GetTypeName(@this), innerExceptionCounter, FormatExceptionLog(exception));
+                innerException = innerException.InnerException;
+            }
+        }
+
+        public static void LogWarning(this IDebugLogger @this, object message)
+        {
+            Debug.LogFormat("[{0}] Warning: {1}", GetTypeName(@this), message);
         }
     }
 }
