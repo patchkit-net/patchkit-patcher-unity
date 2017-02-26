1fa06a3a5b724c77aa30833392a43e09dff1a3e9
Tomasz Jaworski <tomasz.jaworski98@gmail.com>
2017-02-26 21:29:39 +0100

    Improve logging of AppUpdaterEmptyStrategy

--- src/Assets/Scripts/AppUpdater/AppUpdaterEmptyStrategy.cs
+++ src/Assets/Scripts/AppUpdater/AppUpdaterEmptyStrategy.cs
@@ -1,11 +1,15 @@
 ﻿using PatchKit.Unity.Patcher.Cancellation;
+using PatchKit.Unity.Patcher.Debug;
 
 namespace PatchKit.Unity.Patcher.AppUpdater
 {
     public class AppUpdaterEmptyStrategy : IAppUpdaterStrategy
     {
+        private static readonly DebugLogger DebugLogger = new DebugLogger(typeof(AppUpdaterStrategyResolver));
+
         public void Update(CancellationToken cancellationToken)
         {
+            DebugLogger.Log("Updating with empty strategy. Doing nothing. ");
         }
     }
 }
