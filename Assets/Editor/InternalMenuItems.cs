53cbd6ef9202738ddd8e3f401bbb3995e2c3a204
Piotr Korzuszek <piotr.korzuszek@gmail.com>
2017-04-01 09:39:12 +0200

    Asset Store tools and internal tools
    These tools are used to prepare and publish the package

---
+++ Assets/Editor/InternalMenuItems.cs
@@ -0,0 +1,31 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace PatchKit.Unity.Editor
+{
+
+public class InternalMenuItems
+{
+    [MenuItem("Tools/PatchKit Patcher Internal/Upload Asset Store Package", false, 1)]
+    public static void UploadAssetStorePackage()
+    {
+        if (string.IsNullOrEmpty(InternalSettings.Email) || string.IsNullOrEmpty(InternalSettings.Password))
+        {
+            Debug.LogError("Please set email and password in internal settings");
+            return;
+        }
+
+        AssetStoreBatchMode.UploadAssetStorePackage(
+            InternalSettings.Email, InternalSettings.Password,
+            "PatchKit Patcher",
+            new [] {"PatchKit Patcher", "Plugins/PatchKit", "Plugins/UniRx", "StreamingAssets"});
+    }
+
+    [MenuItem("Tools/PatchKit Patcher Internal/Settings", false, 100)]
+    public static void Settings()
+    {
+        InternalSettings.Show();
+    }
+}
+
+} // namespace
---
+++ Assets/Editor/InternalSettings.cs
@@ -0,0 +1,38 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace PatchKit.Unity.Editor
+{
+public class InternalSettings : EditorWindow
+{
+    private const string EmailKey = "pk_as_email";
+    private const string PasswordKey = "pk_as_password";
+
+    public static string Email
+    {
+        get { return EditorPrefs.GetString(EmailKey); }
+        set { EditorPrefs.SetString(EmailKey, value); }
+    }
+
+    public static string Password
+    {
+        get { return EditorPrefs.GetString(PasswordKey); }
+        set { EditorPrefs.SetString(PasswordKey, value); }
+    }
+
+    public static void Show()
+    {
+        EditorWindow window = GetWindow<InternalSettings>();
+        window.Show();
+    }
+
+    private void OnGUI()
+    {
+        EditorGUILayout.LabelField("Provide Asset Store username and password.");
+
+        Email = EditorGUILayout.TextField("Email", Email);
+        Password = EditorGUILayout.PasswordField("Password", Password);
+    }
+}
+
+} // namespace
