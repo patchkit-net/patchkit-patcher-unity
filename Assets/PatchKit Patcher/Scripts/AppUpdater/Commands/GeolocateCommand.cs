5e863b4491bed7198e4a792fbf1528ebe85d111b
Piotr Korzuszek <piotr.korzuszek@gmail.com>
2017-07-18 22:11:04 +0200

    Geolocate command reads current country code

--- Assets/PatchKit Patcher/Scripts/AppUpdater/Commands/GeolocateCommand.cs
+++ Assets/PatchKit Patcher/Scripts/AppUpdater/Commands/GeolocateCommand.cs
@@ -1,18 +1,25 @@
 ﻿using System;
+using System.Collections;
+using System.Diagnostics;
 using System.Linq;
 using System.Net;
 using System.Net.Security;
 using System.Security.Cryptography.X509Certificates;
+using I18N.West;
 using Newtonsoft.Json.Linq;
 using PatchKit.Api;
 using PatchKit.Unity.Patcher.Cancellation;
 using PatchKit.Unity.Patcher.Debug;
 using PatchKit.Unity.Patcher.Status;
+using PatchKit.Unity.Utilities;
+using UnityEngine;
 
 namespace PatchKit.Unity.Patcher.AppUpdater.Commands
 {
     public class GeolocateCommand : IGeolocateCommand
     {
+        private const int Timeout = 5000;
+        
         private static readonly DebugLogger DebugLogger = new DebugLogger(typeof(GeolocateCommand));
         
         public string CountryCode { get; private set; }
@@ -22,13 +29,47 @@ namespace PatchKit.Unity.Patcher.AppUpdater.Commands
         public void Execute(CancellationToken cancellationToken)
         {
             DebugLogger.Log("Trying to geolocate current host...");
+            
             try
             {
+
+                string responseString = null;
+                JToken jToken = null;
+                
+#if UNITY_STANDALONE
+                var eventWaitHandle = UnityDispatcher.Invoke(() =>
+                {
+                    var www = new WWW("https://ip2loc.patchkit.net/v1/country");
+                    var stopwatch = new Stopwatch();
+                    stopwatch.Start();
+
+                    while (!www.isDone)
+                    {
+                        if (stopwatch.ElapsedMilliseconds >= Timeout)
+                        {
+                            break;
+                        }
+                    }
+
+                    if (!www.isDone)
+                    {
+                        DebugLogger.LogError("Timeout while getting country code");
+                        return;
+                    }
+
+                    responseString = www.text;
+                    jToken = JToken.Parse(www.text);
+                });
+                eventWaitHandle.WaitOne();
+
+#else
+                ServicePointManager.ServerCertificateValidationCallback += (sender, certificate, chain, errors) => true;
+    
                 var apiConnectionSettings = new ApiConnectionSettings
                 {
                     CacheServers = new string[0],
                     MainServer = "ip2loc.patchkit.net",
-                    Timeout = 10000,
+                    Timeout = Timeout,
                     UseHttps = true,
                     Port = 443
                 };
@@ -37,9 +78,12 @@ namespace PatchKit.Unity.Patcher.AppUpdater.Commands
                 DebugLogger.Log("aaa");
                 ServicePointManager.ServerCertificateValidationCallback += CertificateValidationCallBack;
                 var countryResponse = apiConnection.GetResponse("/v1/country", null);
-                JToken jToken = countryResponse.GetJson();
+    
+                responseString = countryResponse.Body;
+                jToken = countryResponse.GetJson();
+#endif
                 
-                if (jToken.Contains("country"))
+                if (jToken.Value<string>("country") != null)
                 {
                     CountryCode = jToken.Value<string>("country");
                     HasCountryCode = !string.IsNullOrEmpty(CountryCode);
@@ -55,62 +99,13 @@ namespace PatchKit.Unity.Patcher.AppUpdater.Commands
                 }
                 else
                 {
-                    DebugLogger.LogErrorFormat("Cannot find 'country' key in response json: {0}", countryResponse.Body);
+                    DebugLogger.LogErrorFormat("Cannot find 'country' key in response json: {0}", responseString);
                 }
             }
             catch (Exception ex)
             {
                 DebugLogger.LogErrorFormat("Error while trying to geolocate: " + ex.Message);
                 DebugLogger.LogException(ex);
-            }
-        }
-
-        private bool CertificateValidationCallBack(object sender, X509Certificate certificate, X509Chain chain,
-            SslPolicyErrors sslPolicyErrors)
-        {
-            DebugLogger.Log("HERE");
-            // If the certificate is a valid, signed certificate, return true.
-            if (sslPolicyErrors == System.Net.Security.SslPolicyErrors.None)
-            {
-                return true;
-            }
-
-            // If there are errors in the certificate chain, look at each error to determine the cause.
-            if ((sslPolicyErrors & System.Net.Security.SslPolicyErrors.RemoteCertificateChainErrors) != 0)
-            {
-                if (chain != null && chain.ChainStatus != null)
-                {
-                    foreach (System.Security.Cryptography.X509Certificates.X509ChainStatus status in chain.ChainStatus)
-                    {
-                        if ((certificate.Subject == certificate.Issuer) &&
-                            (status.Status == System.Security.Cryptography.X509Certificates.X509ChainStatusFlags
-                                 .UntrustedRoot))
-                        {
-                            // Self-signed certificates with an untrusted root are valid. 
-                            continue;
-                        }
-                        else
-                        {
-                            if (status.Status != System.Security.Cryptography.X509Certificates.X509ChainStatusFlags
-                                    .NoError)
-                            {
-                                // If there are any other errors in the certificate chain, the certificate is invalid,
-                                // so the method returns false.
-                                return false;
-                            }
-                        }
-                    }
-                }
-
-                // When processing reaches this line, the only errors in the certificate chain are 
-                // untrusted root errors for self-signed certificates. These certificates are valid
-                // for default Exchange server installations, so return true.
-                return true;
-            }
-            else
-            {
-                // In all other cases, return false.
-                return false;
             }
         }
 
