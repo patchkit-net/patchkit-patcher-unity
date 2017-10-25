144d564b2d931031d44257cec0cd9c33f8bf107c
Tomasz Jaworski <tomasz.jaworski98@gmail.com>
2017-10-25 20:45:11 +0200

    Add information about secret to sentry message

--- Assets/PatchKit Patcher/Scripts/Debug/PatcherLogSentryRegistry.cs
+++ Assets/PatchKit Patcher/Scripts/Debug/PatcherLogSentryRegistry.cs
@@ -28,8 +28,18 @@ namespace PatchKit.Unity.Patcher.Debug
 
         private static SentryMessage CreateSentryMessage(string logFileGuid)
         {
-            var msg = string.Format(
-                "Log: https://s3-us-west-2.amazonaws.com/patchkit-app-logs/patcher-unity/2017_04_03/{0}.201-log.gz", logFileGuid);
+            var logFileLink = string.Format(
+                "https://s3-us-west-2.amazonaws.com/patchkit-app-logs/patcher-unity/2017_04_03/{0}.201-log.gz", logFileGuid);
+
+
+            var appSecret = "{no secret}";
+            var patcher = Patcher.Instance;
+            if (patcher != null && patcher.Data.Value.AppSecret != null)
+            {
+                appSecret = Patcher.Instance.Data.Value.AppSecret;
+            }
+
+            var msg = string.Format("Log: {0}\nApp secret: {1}", logFileLink, appSecret);
 
             return new SentryMessage(msg);
         }
