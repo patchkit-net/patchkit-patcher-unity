f914ead0334dd3c79461d68e0cbbc48551b6bf03
Michal Klos <michal.m.klos@gmail.com>
2017-11-28 21:44:24 +0100

    calc eta based on connection speed

--- Assets/PatchKit Patcher/Scripts/UI/DownloadSpeed.cs
+++ Assets/PatchKit Patcher/Scripts/UI/DownloadSpeed.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.UI;
 
 namespace PatchKit.Unity.Patcher.UI
@@ -11,10 +12,32 @@ namespace PatchKit.Unity.Patcher.UI
         {
             Patcher.Instance.UpdateAppStatusChanged += status =>
             {
-                Text.text = status.IsDownloading ? (status.DownloadBytesPerSecond / 1024.0).ToString("0.0 kB/sec.") : string.Empty;
+                string speed = (status.DownloadBytesPerSecond / 1024.0).ToString("0.0 kB/sec.");
+                string remaining = GetRemainingTimeFormated(GetRemainingTime(status));
+
+                string eta = status.DownloadBytesPerSecond > 0.0 ? " ETA: " + remaining : string.Empty;
+                Text.text = status.IsDownloading ? speed + eta : string.Empty;
             };
 
             Text.text = string.Empty;
+        }
+
+        private float GetRemainingTime(Unity.Patcher.Status.OverallStatus status)
+        {
+            float remainingBytes = status.DownloadTotalBytes - status.DownloadBytes;
+            return remainingBytes / (float)status.DownloadBytesPerSecond;
+        }
+
+        private string GetRemainingTimeFormated(float remainingTime)
+        {
+            string result = string.Empty;
+            TimeSpan t = TimeSpan.FromSeconds(remainingTime);
+
+            result += t.Hours > 0 ? string.Format("{0:D2}h:", t.Hours) : string.Empty;
+            result += t.Minutes > 0 ? string.Format("{0:D2}m:", t.Minutes) : string.Empty;
+            result += t.Seconds > 0 ? string.Format("{0:D2}s", t.Seconds) : string.Empty;
+
+            return result;
         }
     }
 }
