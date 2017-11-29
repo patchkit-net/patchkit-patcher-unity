2d6a7f6ff56efe59d521303427d6cd6e512d40cf
Michal Klos <michal.m.klos@gmail.com>
2017-11-29 02:16:15 +0100

    added time estimation next to download speed counter

--- Assets/PatchKit Patcher/Scripts/UI/DownloadSpeed.cs
+++ Assets/PatchKit Patcher/Scripts/UI/DownloadSpeed.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using PatchKit.Unity.Patcher.Status;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -13,31 +14,34 @@ namespace PatchKit.Unity.Patcher.UI
             Patcher.Instance.UpdateAppStatusChanged += status =>
             {
                 string speed = (status.DownloadBytesPerSecond / 1024.0).ToString("0.0 kB/sec.");
-                string remaining = GetRemainingTimeFormated(GetRemainingTime(status));
+                string eta = (status.DownloadBytes > 0 && status.DownloadBytes < status.DownloadTotalBytes)
+                    ? " ready in " + GetRemainingTimeFormated(GetRemainingTime(status))
+                    : string.Empty;
 
-                string eta = status.DownloadBytesPerSecond > 0.0 ? " ETA: " + remaining : string.Empty;
                 Text.text = status.IsDownloading ? speed + eta : string.Empty;
             };
 
             Text.text = string.Empty;
         }
 
-        private float GetRemainingTime(Unity.Patcher.Status.OverallStatus status)
+        private float GetRemainingTime(OverallStatus status)
         {
             float remainingBytes = status.DownloadTotalBytes - status.DownloadBytes;
             return remainingBytes / (float)status.DownloadBytesPerSecond;
         }
 
-        private string GetRemainingTimeFormated(float remainingTime)
+        private string GetRemainingTimeFormated(float value)
         {
-            string result = string.Empty;
-            TimeSpan t = TimeSpan.FromSeconds(remainingTime);
+            TimeSpan span = TimeSpan.FromSeconds(value);
+            return span.Days > 0 ? string.Format("{0:0} day", span.Days) + GetPlural(span.Days) :
+                        span.Hours > 0 ? string.Format("{0:0} hour", span.Hours) + GetPlural(span.Hours) :
+                            span.Minutes > 0 ? string.Format("{0:0} minute", span.Minutes) + GetPlural(span.Minutes) :
+                                span.Seconds > 0 ? string.Format("{0:0} second", span.Seconds) + GetPlural(span.Seconds) : "a moment";
+        }
 
-            result += t.Hours > 0 ? string.Format("{0:D2}h:", t.Hours) : string.Empty;
-            result += t.Minutes > 0 ? string.Format("{0:D2}m:", t.Minutes) : string.Empty;
-            result += t.Seconds > 0 ? string.Format("{0:D2}s", t.Seconds) : string.Empty;
-
-            return result;
+        private string GetPlural(int value)
+        {
+            return value == 1 ? string.Empty : "s";
         }
     }
 }
