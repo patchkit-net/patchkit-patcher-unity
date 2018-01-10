ee2501f3328eed5b6af1af359e9a3dd0bdb9c8a8
Tomasz Jaworski <tomasz.jaworski98@gmail.com>
2018-01-10 22:56:34 +0100

    Add download speed unit switching

--- Assets/PatchKit Patcher/Scripts/UI/DownloadSpeed.cs
+++ Assets/PatchKit Patcher/Scripts/UI/DownloadSpeed.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using PatchKit.Unity.Patcher.Status;
+using UniRx;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -13,8 +15,8 @@ namespace PatchKit.Unity.Patcher.UI
         {
             Patcher.Instance.UpdateAppStatusChanged += status =>
             {
-                string speed = (status.DownloadBytesPerSecond / 1024.0).ToString("0.0 kB/sec.");
-                string eta = (status.DownloadBytes > 0 && status.DownloadBytes < status.DownloadTotalBytes)
+                string speed = GetDownloadSpeedFormated(status);
+                string eta = status.DownloadBytes > 0 && status.DownloadBytes < status.DownloadTotalBytes
                     ? " ready in " + GetRemainingTimeFormated(GetRemainingTime(status))
                     : string.Empty;
 
@@ -24,19 +26,43 @@ namespace PatchKit.Unity.Patcher.UI
             Text.text = string.Empty;
         }
 
+        private string GetDownloadSpeedFormated(OverallStatus status)
+        {
+            double kbPerSecond = status.DownloadBytesPerSecond / 1024.0;
+
+            return kbPerSecond > 1024.0
+                ? FormatDownloadSpeedMegabytes(kbPerSecond)
+                : FormatDownloadSpeedKilobytes(kbPerSecond);
+        }
+
+        private string FormatDownloadSpeedMegabytes(double kbPerSecond)
+        {
+            return FormatDownloadSpeed(kbPerSecond / 1024.0) + " MB/sec.";
+        }
+
+        private string FormatDownloadSpeedKilobytes(double kbPerSecond)
+        {
+            return FormatDownloadSpeed(kbPerSecond) + " KB/sec.";
+        }
+
+        private string FormatDownloadSpeed(double s)
+        {
+            return s.ToString("#,#0.0");
+        }
+
         private float GetRemainingTime(OverallStatus status)
         {
             float remainingBytes = status.DownloadTotalBytes - status.DownloadBytes;
-            return remainingBytes / (float)status.DownloadBytesPerSecond;
+            return remainingBytes / (float) status.DownloadBytesPerSecond;
         }
 
         private string GetRemainingTimeFormated(float value)
         {
             TimeSpan span = TimeSpan.FromSeconds(value);
             return span.Days > 0 ? string.Format("{0:0} day", span.Days) + GetPlural(span.Days) :
-                        span.Hours > 0 ? string.Format("{0:0} hour", span.Hours) + GetPlural(span.Hours) :
-                            span.Minutes > 0 ? string.Format("{0:0} minute", span.Minutes) + GetPlural(span.Minutes) :
-                                span.Seconds > 0 ? string.Format("{0:0} second", span.Seconds) + GetPlural(span.Seconds) : "a moment";
+                span.Hours > 0 ? string.Format("{0:0} hour", span.Hours) + GetPlural(span.Hours) :
+                span.Minutes > 0 ? string.Format("{0:0} minute", span.Minutes) + GetPlural(span.Minutes) :
+                span.Seconds > 0 ? string.Format("{0:0} second", span.Seconds) + GetPlural(span.Seconds) : "a moment";
         }
 
         private string GetPlural(int value)
