8e07664699a0462a0e196a24f8ff72eb6a09c240
Dzejkop <jakubtrad@gmail.com>
2018-02-13 18:38:30 +0100

    Updating the build scripts to use new version value.

--- Assets/PatchKit Patcher/Editor/PatcherVersionInfoCreator.cs
+++ Assets/PatchKit Patcher/Editor/PatcherVersionInfoCreator.cs
@@ -25,64 +25,7 @@ namespace PatchKit.Unity.Editor
 
         private static string GetVersionInfo()
         {
-            var commitHash = GetCommitHash();
-            var commitTags = GetCommitTags();
-            var branchName = GetBranchName();
-
-            return string.Format("{0} (tags: {1}, branch: {2})", commitHash, commitTags, branchName);
-        }
-
-        private static string GetBranchName()
-        {
-            string output;
-            if (RunGitCommand("symbolic-ref HEAD", out output))
-            {
-                return output.TrimEnd('\n');
-            }
-
-            return "(unknown)";
-        }
-
-        private static string GetCommitTags()
-        {
-            string output;
-            if (RunGitCommand("describe --abbrev=0 --tags --exact-match", out output))
-            {
-                return output.TrimEnd('\n');
-            }
-
-            return "(unknown)";
-        }
-
-        private static string GetCommitHash()
-        {
-            string output;
-            if (RunGitCommand("rev-parse HEAD", out output))
-            {
-                return output.TrimEnd('\n');
-            }
-
-            return "(unknown)";
-        }
-
-        private static bool RunGitCommand(string command, out string output)
-        {
-            var process = new Process
-            {
-                StartInfo =
-                {
-                    FileName = "git",
-                    Arguments = command,
-                    RedirectStandardOutput = true,
-                    UseShellExecute = false
-                }
-            };
-
-            process.Start();
-            output = process.StandardOutput.ReadToEnd();
-            process.WaitForExit(1000);
-
-            return process.ExitCode == 0;
+            return Patcher.Version.Value;
         }
     }
 }
