3cb918dd68adae7283adbbc98405272712d83328
Dzejkop <jakubtrad@gmail.com>
2018-02-14 11:32:15 +0100

    PatchKit logo script

---
+++ Assets/PatchKit Patcher/Scripts/PatchKitLogo.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UniRx;
+using PatchKit.Unity.Patcher.Debug;
+
+namespace PatchKit.Unity
+{
+
+    public class PatchKitLogo : MonoBehaviour
+    {
+        [SerializeField]
+        private string patchKitWebsiteUrl;
+
+        [SerializeField]
+        private Texture2D cursorTexture;
+
+        [SerializeField]
+        private Vector2 cursorHotspot;
+
+        [SerializeField]
+        private Image image;
+
+        [SerializeField]
+        private Button button;
+
+        void Awake()
+        {
+            Assert.IsNotNull(button);
+            Assert.IsNotNull(image);
+
+            Assert.IsNotNull(cursorTexture);
+
+            button.enabled = false;
+            image.enabled = false;
+        }
+
+        void Start()
+        {
+            var patcher = Patcher.Patcher.Instance;
+
+            patcher.AppInfo
+                .ObserveOnMainThread()
+                .Subscribe(app => Resolve(app.PatcherWhitelabel))
+                .AddTo(this);
+        }
+
+        private void Resolve(bool isWhitelabel)
+        {
+            image.enabled = !isWhitelabel;
+            button.enabled = !isWhitelabel;
+        }
+
+        public void OnMouseEnter()
+        {
+            Cursor.SetCursor(cursorTexture, cursorHotspot, CursorMode.Auto);
+        }
+
+        public void OnMouseExit()
+        {
+            Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+        }
+
+        public void GoToPatchKit()
+        {
+            Application.OpenURL(patchKitWebsiteUrl);
+        }
+    }
+}
