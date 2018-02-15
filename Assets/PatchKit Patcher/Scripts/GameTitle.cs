aba9940f9b6b01462474fff9e692f8ab97fd462d
Dzejkop <jakubtrad@gmail.com>
2018-02-15 14:54:52 +0100

    Game title script

---
+++ Assets/PatchKit Patcher/Scripts/GameTitle.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+using PatchKit.Unity.Patcher.Debug;
+
+namespace PatchKit.Unity
+{
+    public class GameTitle : MonoBehaviour
+    {
+        [SerializeField]
+        private Text targetText;
+
+        void Start()
+        {
+            var patcher = Patcher.Patcher.Instance;
+
+            Assert.IsNotNull(patcher);
+
+            patcher.AppInfo
+                .ObserveOnMainThread()
+                .Subscribe(OnInfoUpdate)
+                .AddTo(this);
+        }
+
+        private void OnInfoUpdate(Api.Models.Main.App appInfo)
+        {
+            if (!string.IsNullOrEmpty(appInfo.DisplayName))
+            {
+                targetText.text = appInfo.DisplayName;
+            }
+        }
+    }
+}
