ebfb4d05b6b2716ff0851b1ed16473e8c1bcd008
Dzejkop <jakubtrad@gmail.com>
2018-02-15 15:07:15 +0100

    Custom editor to display inspector warning and button.

---
+++ Assets/PatchKit Patcher/Editor/PatcherEditor.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+
+namespace PatchKit.Unity.Patcher
+{
+    [CustomEditor(typeof(Patcher))]
+    public class PatcherEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            
+            Patcher patcher = (Patcher) target;
+
+            if (patcher.EditorAppSecret != Patcher.EditorAllowedSecret)
+            {
+                EditorGUILayout.HelpBox("Reset the editor app secret before building.", MessageType.Warning);
+                if(GUILayout.Button("Reset"))
+                {
+                    patcher.EditorAppSecret = Patcher.EditorAllowedSecret;
+                }
+            }
+        }
+    }
+}
