94b956ea54dabdec693581b5febb80b46e720b0d
Dzejkop <jakubtrad@gmail.com>
2018-02-21 12:53:20 +0100

    Updating the tests

--- Assets/Editor/Tests/ChunkedHttpDownloaderTest.cs
+++ Assets/Editor/Tests/ChunkedHttpDownloaderTest.cs
@@ -21,6 +21,31 @@ public class ChunkedHttpDownloaderTest
     private ResourceUrl _url;
     private ChunksData _chunksData;
 
+    [Test]
+    public void ByteRangesTests()
+    {
+        var fullRange = BytesRangeUtils.Full();
+        var outer = BytesRangeUtils.Make(0, 100);
+        var inner = BytesRangeUtils.Make(10, 90);
+        var innerInside = BytesRangeUtils.Make(50, 60);
+        var innerRightSideOverlap = BytesRangeUtils.Make(50, 95);
+        var innerLeftSideOverlap = BytesRangeUtils.Make(5, 15);
+
+        Assert.That(outer.Contains(inner));
+
+        var localizedRightOverlap = innerRightSideOverlap.LocalizeTo(inner);
+        var localizedLeftOverlap = innerLeftSideOverlap.LocalizeTo(inner);
+
+        Assert.That(localizedRightOverlap, Is.EqualTo(BytesRangeUtils.Make(40, -1)));
+        Assert.That(localizedLeftOverlap, Is.EqualTo(BytesRangeUtils.Make(0, 5)));
+
+        var localizedFull = fullRange.LocalizeTo(inner);
+        Assert.That(localizedFull, Is.EqualTo(BytesRangeUtils.Full()));
+
+        var localizedInner = innerInside.LocalizeTo(inner);
+        Assert.That(localizedInner, Is.EqualTo(BytesRangeUtils.Make(40, 50)));
+    }
+
     [SetUp]
     public void SetUp()
     {
@@ -52,58 +77,48 @@ public class ChunkedHttpDownloaderTest
     [Test]
     public void ChunksCalculations_WithSpecifiedRange_1()
     {
-        BytesRange range = new BytesRange 
-        {
-            Start = 9,
-            End = 315
-        };
+        BytesRange range = BytesRangeUtils.Make(9, 315);
 
-        var chunksRange = ChunkedHttpDownloader.CalculateContainingChunksRange(range, _chunksData);
+        var chunksRange =range.Chunkify(_chunksData);
 
         Assert.That(chunksRange.Start, Is.EqualTo(0));
-        Assert.That(chunksRange.End, Is.EqualTo(ChunkSize * 5));
+        Assert.That(chunksRange.End, Is.EqualTo(ChunkSize * 5 - 1));
+
+        Assert.That(chunksRange.Contains(range));
     }
 
     [Test]
     public void ChunksCalculations_WithSpecifiedRange_2()
     {
-        BytesRange range = new BytesRange 
-        {
-            Start = 450,
-            End = 830
-        };
+        BytesRange range = BytesRangeUtils.Make(450, 830);
 
-        var chunksRange = ChunkedHttpDownloader.CalculateContainingChunksRange(range, _chunksData);
+        var chunksRange = range.Chunkify(_chunksData);
 
         Assert.That(chunksRange.Start, Is.EqualTo(ChunkSize * 7));
-        Assert.That(chunksRange.End, Is.EqualTo(ChunkSize * 13));
+        Assert.That(chunksRange.End, Is.EqualTo(ChunkSize * 13 - 1));
+
+        Assert.That(chunksRange.Contains(range));
     }
 
     [Test]
     public void ChunksCalculations_WithFullRange()
     {
-        BytesRange range = new BytesRange 
-        {
-            Start = 0,
-            End = -1
-        };
+        BytesRange range = BytesRangeUtils.Full();
 
-        var chunksRange = ChunkedHttpDownloader.CalculateContainingChunksRange(range, _chunksData);
+        var chunksRange = range.Chunkify(_chunksData);
 
         Assert.That(chunksRange.Start, Is.EqualTo(0));
         Assert.That(chunksRange.End, Is.EqualTo(-1));
+
+        Assert.That(chunksRange.Contains(range));
     }
 
     [Test]
     public void ChunksCalculations_WithRangeExactlyAsDataSize()
     {
-        BytesRange range = new BytesRange 
-        {
-            Start = 0,
-            End = DataSize
-        };
+        BytesRange range = BytesRangeUtils.Make(0, DataSize);
 
-        var chunksRange = ChunkedHttpDownloader.CalculateContainingChunksRange(range, _chunksData);
+        var chunksRange = range.Chunkify(_chunksData);
 
         Assert.That(chunksRange.Start, Is.EqualTo(0));
         Assert.That(chunksRange.End, Is.EqualTo(DataSize));
@@ -112,13 +127,9 @@ public class ChunkedHttpDownloaderTest
     [Test]
     public void JobQueuing_WithFullRange()
     {
-        BytesRange range = new BytesRange 
-        {
-            Start = 0,
-            End = -1
-        };
+        BytesRange range = BytesRangeUtils.Full();
 
-        var jobs = ChunkedHttpDownloader.BuildDownloadJobQueue(_url, 0,range, DataSize, _chunksData).ToList();
+        var jobs = ChunkedHttpDownloader.BuildDownloadJobQueue(_url, 0, range, DataSize, _chunksData).ToList();
         int expectedJobCount = (int) (DataSize / PartSize) + 1;
 
         Assert.That(jobs.Count, Is.EqualTo(expectedJobCount));
@@ -137,11 +148,7 @@ public class ChunkedHttpDownloaderTest
     [Test]
     public void JobQueuing_WithSpecifiedRange()
     {
-        BytesRange range = new BytesRange 
-        {
-            Start = 450,
-            End = 830
-        };
+        BytesRange range = BytesRangeUtils.Make(450, 830);
 
         var jobs = ChunkedHttpDownloader.BuildDownloadJobQueue(_url, 0,range, DataSize, _chunksData).ToList();
 
@@ -163,8 +170,6 @@ public class ChunkedHttpDownloaderTest
 
         Assert.That(lastJob.Url, Is.EqualTo("test.com/someData.6"));
 
-        // NOTE: The range 0-63 essentialy means "Download bytes with indices 0 to 63" so 64 bytes in total
-        // NOTE: Yes, this is confusing, and it's because the interpretation of BytesRange for HttpDownloader is different to others
         Assert.That(lastJob.Range.Start, Is.EqualTo(0));
         Assert.That(lastJob.Range.End, Is.EqualTo(63));
     }
@@ -172,11 +177,7 @@ public class ChunkedHttpDownloaderTest
     [Test]
     public void JobQueuing_WithSpecifiedRangeAndOffset()
     {
-        BytesRange range = new BytesRange 
-        {
-            Start = 450,
-            End = 830
-        };
+        BytesRange range = BytesRangeUtils.Make(450, 830);
 
         long offset = 512;
 
@@ -207,11 +208,7 @@ public class ChunkedHttpDownloaderTest
     [Test]
     public void JobQueuing_SinglePartScenario()
     {
-        BytesRange range = new BytesRange 
-        {
-            Start = 315,
-            End = 380
-        };
+        BytesRange range = BytesRangeUtils.Make(315, 380);
 
         var jobs = ChunkedHttpDownloader.BuildDownloadJobQueue(_url, 0,range, DataSize, _chunksData).ToList();
 
@@ -221,6 +218,6 @@ public class ChunkedHttpDownloaderTest
 
         Assert.That(job.Url, Is.EqualTo("test.com/someData.2"));
         Assert.That(job.Range.Start, Is.EqualTo(0));
-        Assert.That(job.Range.End, Is.EqualTo(-1).Or.EqualTo(127)); // 127 and -1 both denote the full part
+        Assert.That(job.Range.End, Is.EqualTo(-1).Or.EqualTo(127));
     }
 }
