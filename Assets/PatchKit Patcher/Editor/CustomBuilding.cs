2b25afb0e9ccd0a67b93ab16b66a53a81cc58673
Dzejkop <jakubtrad@gmail.com>
2018-02-28 15:43:24 +0100

    Custom build script

---
+++ Assets/PatchKit Patcher/Editor/CustomBuilding.cs
@@ -0,0 +1,107 @@
+// C# example.
+using UnityEditor;
+using System.Diagnostics;
+
+public class ScriptBatch 
+{
+    [MenuItem("Tools/Build/Windows x86")]
+    public static void BuildWindows86 ()
+    {
+        Build(BuildTarget.StandaloneWindows);
+    }
+
+    [MenuItem("Tools/Build/Windows x64")]
+    public static void BuildWindows64 ()
+    {
+        Build(BuildTarget.StandaloneWindows64);
+    }
+
+    [MenuItem("Tools/Build/Linux x86")]
+    public static void BuildLinux86 ()
+    {
+        Build(BuildTarget.StandaloneLinux);
+    }
+
+    [MenuItem("Tools/Build/Linux x64")]
+    public static void BuildLinux64 ()
+    {
+        Build(BuildTarget.StandaloneLinux64);
+    }
+
+    [MenuItem("Tools/Build/Linux Universal")]
+    public static void BuildLinux ()
+    {
+        Build(BuildTarget.StandaloneLinuxUniversal);
+    }
+
+    [MenuItem("Tools/Build/OSX")]
+    public static void BuildOsx ()
+    {
+        Build(BuildTarget.StandaloneOSXIntel);
+    }
+
+    [MenuItem("Tools/Build/OSX x64")]
+    public static void BuildOsx64 ()
+    {
+        Build(BuildTarget.StandaloneOSXIntel64);
+    }
+
+    [MenuItem("Tools/Build/OSX Universal")]
+    public static void BuildOsxUniversal ()
+    {
+        Build(BuildTarget.StandaloneOSXUniversal);
+    }
+
+    private static string PatcherExecutableName(BuildTarget target)
+    {
+        switch (target)
+        {
+            case BuildTarget.StandaloneWindows:
+            case BuildTarget.StandaloneWindows64:
+                return "Patcher.exe";
+
+            case BuildTarget.StandaloneLinux:
+            case BuildTarget.StandaloneLinux64:
+            case BuildTarget.StandaloneLinuxUniversal:
+                return "Patcher";
+
+            case BuildTarget.StandaloneOSXIntel:
+            case BuildTarget.StandaloneOSXIntel64:
+            case BuildTarget.StandaloneOSXUniversal:
+                return "Patcher.app";
+
+            default:
+                return "";
+        }
+    }
+
+    private static void Build(BuildTarget target)
+    {
+        string path = EditorUtility.SaveFolderPanel("Choose the location of build Patcher", "", "");
+        var patcher = PatchKit.Unity.Patcher.Patcher.Instance;
+
+        if (patcher.EditorAppSecret != PatchKit.Unity.Patcher.Patcher.EditorAllowedSecret)
+        {
+            if (EditorUtility.DisplayDialog("Error", "Please reset the editor app secret to continue building.", "Reset the secret and continue", "Cancel"))
+            {
+                patcher.EditorAppSecret = PatchKit.Unity.Patcher.Patcher.EditorAllowedSecret;
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        string[] levels = new string[] {EditorApplication.currentScene};
+
+        BuildOptions buildOptions = BuildOptions.ForceEnableAssertions 
+                                  | BuildOptions.ShowBuiltPlayer;
+
+        string error = BuildPipeline.BuildPlayer(levels, path + "/" + PatcherExecutableName(target), target, buildOptions);
+
+        if (!string.IsNullOrEmpty(error))
+        {
+            EditorUtility.DisplayDialog("Error", error, "Ok");
+        }
+    }
+}
