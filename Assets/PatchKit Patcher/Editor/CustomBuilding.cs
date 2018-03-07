804de9adb05bbc716a8bfd0c0d9fef7894f5d358
Dzejkop <jakubtrad@gmail.com>
2018-03-07 11:46:02 +0100

    Better custom building
    The script will now look for a Patcher instance in on of the build scenes.

--- Assets/PatchKit Patcher/Editor/CustomBuilding.cs
+++ Assets/PatchKit Patcher/Editor/CustomBuilding.cs
@@ -1,123 +1,154 @@
-// C# example.
 using UnityEditor;
-using System.Diagnostics;
+using UnityEditor.SceneManagement;
 using System.Linq;
+using System.Diagnostics;
 
-public class ScriptBatch 
+namespace PatchKit.Unity
 {
-    [MenuItem("Tools/Build/Windows x86")]
-    public static void BuildWindows86 ()
+    public class CustomBuildScripts 
     {
-        Build(BuildTarget.StandaloneWindows);
-    }
-
-    [MenuItem("Tools/Build/Windows x64")]
-    public static void BuildWindows64 ()
-    {
-        Build(BuildTarget.StandaloneWindows64);
-    }
-
-    [MenuItem("Tools/Build/Linux x86")]
-    public static void BuildLinux86 ()
-    {
-        Build(BuildTarget.StandaloneLinux);
-    }
-
-    [MenuItem("Tools/Build/Linux x64")]
-    public static void BuildLinux64 ()
-    {
-        Build(BuildTarget.StandaloneLinux64);
-    }
-
-    [MenuItem("Tools/Build/Linux Universal")]
-    public static void BuildLinux ()
-    {
-        Build(BuildTarget.StandaloneLinuxUniversal);
-    }
-
-    [MenuItem("Tools/Build/OSX")]
-    public static void BuildOsx ()
-    {
-        Build(BuildTarget.StandaloneOSXIntel);
-    }
-
-    [MenuItem("Tools/Build/OSX x64")]
-    public static void BuildOsx64 ()
-    {
-        Build(BuildTarget.StandaloneOSXIntel64);
-    }
-
-    [MenuItem("Tools/Build/OSX Universal")]
-    public static void BuildOsxUniversal ()
-    {
-        Build(BuildTarget.StandaloneOSXUniversal);
-    }
-
-    private static string PatcherExecutableName(BuildTarget target)
-    {
-        switch (target)
+        [MenuItem("Tools/Build/Windows x86")]
+        public static void BuildWindows86 ()
         {
-            case BuildTarget.StandaloneWindows:
-            case BuildTarget.StandaloneWindows64:
-                return "Patcher.exe";
-
-            case BuildTarget.StandaloneLinux:
-            case BuildTarget.StandaloneLinux64:
-            case BuildTarget.StandaloneLinuxUniversal:
-                return "Patcher";
-
-            case BuildTarget.StandaloneOSXIntel:
-            case BuildTarget.StandaloneOSXIntel64:
-            case BuildTarget.StandaloneOSXUniversal:
-                return "Patcher.app";
-
-            default:
-                return "";
-        }
-    }
-
-    private static void Build(BuildTarget target)
-    {
-        string path = EditorUtility.SaveFolderPanel("Choose where to build the Patcher", "", "");
-
-        if (string.IsNullOrEmpty(path))
-        {
-            return;
+            Build(BuildTarget.StandaloneWindows);
         }
 
-        var patcher = PatchKit.Unity.Patcher.Patcher.Instance;
-
-        if (patcher.EditorAppSecret != PatchKit.Unity.Patcher.Patcher.EditorAllowedSecret)
+        [MenuItem("Tools/Build/Windows x64")]
+        public static void BuildWindows64 ()
         {
-            if (EditorUtility.DisplayDialog("Error", "Please reset the editor app secret to continue building.", "Reset the secret and continue", "Cancel"))
+            Build(BuildTarget.StandaloneWindows64);
+        }
+
+        [MenuItem("Tools/Build/Linux x86")]
+        public static void BuildLinux86 ()
+        {
+            Build(BuildTarget.StandaloneLinux);
+        }
+
+        [MenuItem("Tools/Build/Linux x64")]
+        public static void BuildLinux64 ()
+        {
+            Build(BuildTarget.StandaloneLinux64);
+        }
+
+        [MenuItem("Tools/Build/Linux Universal")]
+        public static void BuildLinux ()
+        {
+            Build(BuildTarget.StandaloneLinuxUniversal);
+        }
+
+        [MenuItem("Tools/Build/OSX")]
+        public static void BuildOsx ()
+        {
+            Build(BuildTarget.StandaloneOSXIntel);
+        }
+
+        [MenuItem("Tools/Build/OSX x64")]
+        public static void BuildOsx64 ()
+        {
+            Build(BuildTarget.StandaloneOSXIntel64);
+        }
+
+        [MenuItem("Tools/Build/OSX Universal")]
+        public static void BuildOsxUniversal ()
+        {
+            Build(BuildTarget.StandaloneOSXUniversal);
+        }
+
+        private static string PatcherExecutableName(BuildTarget target)
+        {
+            switch (target)
             {
-                patcher.EditorAppSecret = PatchKit.Unity.Patcher.Patcher.EditorAllowedSecret;
+                case BuildTarget.StandaloneWindows:
+                case BuildTarget.StandaloneWindows64:
+                    return "Patcher.exe";
+
+                case BuildTarget.StandaloneLinux:
+                case BuildTarget.StandaloneLinux64:
+                case BuildTarget.StandaloneLinuxUniversal:
+                    return "Patcher";
+
+                case BuildTarget.StandaloneOSXIntel:
+                case BuildTarget.StandaloneOSXIntel64:
+                case BuildTarget.StandaloneOSXUniversal:
+                    return "Patcher.app";
+
+                default:
+                    return "";
             }
-            else
+        }
+
+        private static void Build(BuildTarget target)
+        {
+            string[] scenePaths = EditorBuildSettings.scenes
+                .Where(s => s.enabled)
+                .Select(s => s.path)
+                .ToArray();
+
+            var scenes = scenePaths
+                .Select(s => EditorSceneManager.OpenScene(s))
+                .ToArray();
+
+            if (scenePaths.Count() == 0)
+            {
+                EditorUtility.DisplayDialog("Error", "Add or enable scenes to be included in the Build Settings menu.", "Ok");
+                return;
+            }
+
+            EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
+            Patcher.Patcher patcher = null;
+
+            foreach (var scene in scenes)
+            {
+                EditorSceneManager.SetActiveScene(scene);
+
+                patcher = PatchKit.Unity.Patcher.Patcher.Instance;
+
+                if (patcher)
+                {
+                    break;
+                }
+            }
+
+            if (!patcher)
+            {
+                EditorUtility.DisplayDialog("Error", "Couldn't resolve an instance of the Patcher component in any of the build scenes.", "Ok");
+                return;
+            }
+
+            if (patcher.EditorAppSecret != PatchKit.Unity.Patcher.Patcher.EditorAllowedSecret)
+            {
+                if (EditorUtility.DisplayDialog("Error", "Please reset the editor app secret to continue building.", "Reset the secret and continue", "Cancel"))
+                {
+                    patcher.EditorAppSecret = PatchKit.Unity.Patcher.Patcher.EditorAllowedSecret;
+
+                    var activeScene = EditorSceneManager.GetActiveScene();
+
+                    EditorSceneManager.MarkSceneDirty(activeScene);
+                    EditorSceneManager.SaveScene(activeScene);
+                }
+                else
+                {
+                    return;
+                }
+            }
+
+            BuildOptions buildOptions = BuildOptions.ForceEnableAssertions 
+                                    | BuildOptions.ShowBuiltPlayer;
+
+            string path = EditorUtility.SaveFolderPanel("Choose where to build the Patcher", "", "");
+
+            if (string.IsNullOrEmpty(path))
             {
                 return;
             }
-        }
 
-        string[] scenes = EditorBuildSettings.scenes
-            .Where(s => s.enabled)
-            .Select(s => s.path)
-            .ToArray();
+            string error = BuildPipeline.BuildPlayer(scenes, path + "/" + PatcherExecutableName(target), target, buildOptions);
 
-        if (scenes.Count() == 0)
-        {
-            EditorUtility.DisplayDialog("Error", "Add or enable scenes to be included in the Build Settings menu.", "Ok");
-            return;
-        }
-
-        BuildOptions buildOptions = BuildOptions.ForceEnableAssertions 
-                                  | BuildOptions.ShowBuiltPlayer;
-
-        string error = BuildPipeline.BuildPlayer(scenes, path + "/" + PatcherExecutableName(target), target, buildOptions);
-
-        if (!string.IsNullOrEmpty(error))
-        {
-            EditorUtility.DisplayDialog("Error", error, "Ok");
+            if (!string.IsNullOrEmpty(error))
+            {
+                EditorUtility.DisplayDialog("Error", error, "Ok");
+            }
         }
     }
 }
