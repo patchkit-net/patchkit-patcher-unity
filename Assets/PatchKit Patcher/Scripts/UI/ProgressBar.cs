162140ee706bdf171f8430da7768b4b363bb3dcd
Dzejkop <jakubtrad@gmail.com>
2018-03-28 13:17:52 +0200

    Change progress bar behaviour
    The progress bar will only show any kind of status when an operation is in progress. Otherwise (before and after an update) the progress bar will be white with no text.

--- Assets/PatchKit Patcher/Scripts/UI/ProgressBar.cs
+++ Assets/PatchKit Patcher/Scripts/UI/ProgressBar.cs
@@ -13,15 +13,19 @@ namespace PatchKit.Unity.Patcher.UI
 
         private void SetProgress(double progress)
         {
-            Text.text = progress.ToString("0.0%");
+
+            Text.text = progress >= 0.0 ? progress.ToString("0.0%") : "";
+            float visualProgress = progress >= 0.0 ? (float) progress : 0.0f;
+
             var anchorMax = Image.rectTransform.anchorMax;
-            anchorMax.x = (float)progress;
+            anchorMax.x = visualProgress;
             Image.rectTransform.anchorMax = anchorMax;
         }
 
         private void Start()
         {
-            Patcher.Instance.UpdaterStatus.SelectSwitchOrDefault(s => s.Progress, 1.0)
+            Patcher.Instance.UpdaterStatus
+                .SelectSwitchOrDefault(s => s.Progress, -1.0)
                 .ObserveOnMainThread()
                 .Subscribe(SetProgress)
                 .AddTo(this);
