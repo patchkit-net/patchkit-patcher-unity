f353ddc6df2ce2d2590d123767f78966033212b8
Dzejkop <jakubtrad@gmail.com>
2018-04-06 12:02:42 +0200

    Implementation

---
+++ Assets/PatchKit Patcher/Scripts/PatcherEnvironmentVariables.cs
@@ -0,0 +1,7 @@
+﻿namespace PatchKit.Patching.Unity
+{
+    public static class PatcherEnvironmentVariables
+    {
+        public const string KeepTemporaryFilesOnError = "PK_PATCHER_KEEP_FILES_ON_ERROR";
+    }
+}
---
+++ Assets/PatchKit Patcher/Scripts/PatcherTemporaryDirectoryExecutor.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using PatchKit.Core.IO;
+using PatchKit.Apps.Updating.Debug;
+
+namespace PatchKit.Patching.Unity
+{
+    public class UnityTemporaryDirectoryExecutor : ITemporaryDirectoryExecutor
+    {
+        TemporaryDirectory CreateTemporaryDirectory()
+        {
+            var path = Path.Combine(Path.GetTempPath(), Path.GetTempFileName());
+
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+            }
+
+            return new TemporaryDirectory(path);
+        }
+            
+        public void Execute(Action<TemporaryDirectory> action)
+        {
+            using (var tempDir = CreateTemporaryDirectory())
+            {
+                try
+                {
+                    action(tempDir);
+                }
+                catch (Exception e)
+                {
+                    string envValue;
+                    if (EnvironmentInfo.TryReadEnvironmentVariable(PatcherEnvironmentVariables.KeepTemporaryFilesOnError, out envValue))
+                    {
+                        if (envValue != "0")
+                        {
+                            tempDir.Keep = true;
+                        }
+                    }
+                        
+                    throw;
+                }
+            }
+        }
+    }
+}
