86f38a7efa05f5f265dccbb2f77476d21bc162c8
Dzejkop <jakubtrad@gmail.com>
2018-05-17 11:46:51 +0200

    Format implementation

---
+++ Assets/PatchKit Patcher/Editor/Manifest.cs
@@ -0,0 +1,28 @@
+using Newtonsoft.Json;
+
+namespace PatchKit.Unity.Editor
+{
+    public struct Manifest
+    {
+        public struct Argument
+        {
+            [JsonProperty(PropertyName = "value")]
+            public string[] Value;
+        }
+
+        [JsonProperty(PropertyName = "exe_fileName")]
+        public string ExeFileName;
+
+        [JsonProperty(PropertyName = "exe_arguments")]
+        public string ExeArguments;
+
+        [JsonProperty(PropertyName = "manifest_version")]
+        public int Version;
+
+        [JsonProperty(PropertyName = "target")]
+        public string Target;
+
+        [JsonProperty(PropertyName = "target_arguments")]
+        public Argument[] Arguments; 
+    }
+}
