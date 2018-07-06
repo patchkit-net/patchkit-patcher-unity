b9b0eb61db8523a4da20789731df54d43e463d74
Dzejkop <jakubtrad@gmail.com>
2018-07-06 14:39:35 +0200

    Added FileSystem.RetryStrategy

---
+++ Assets/PatchKit Patcher/Scripts/AppData/FileSystem/RetryStrategy.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using PatchKit.Network;
+using PatchKit.Unity.Patcher.Cancellation;
+using PatchKit.Unity.Patcher.Debug;
+using PatchKit.Unity.Utilities;
+
+namespace PatchKit.Unity.Patcher.AppData.FileSystem
+{
+    public class RetryStrategy : IRequestRetryStrategy
+    {
+        private static readonly DebugLogger DebugLogger = new DebugLogger(typeof(RetryStrategy));
+
+        public const int DefaultDuration = 5 * 1000;
+        public const int DefaultDelay = 1000 / 2;
+
+        private readonly int _tryCount;
+        private int _currentTry = 0;
+        private readonly int _delay;
+
+        private RetryStrategy()
+            : this(DefaultDuration, DefaultDelay)
+        {
+        }
+
+        public RetryStrategy(int tryDurationMsec, int delayBetweenEachTryMsec)
+        {
+            _tryCount = tryDurationMsec / delayBetweenEachTryMsec;
+            _delay = delayBetweenEachTryMsec;
+        }
+
+        public void OnRequestSuccess()
+        {
+            // Do nothing
+        }
+
+        public void OnRequestFailure()
+        {
+            _currentTry++;
+        }
+
+        public int DelayBeforeNextTry {
+            get
+            {
+                return _delay;
+            }
+        }
+
+        public bool ShouldRetry
+        {
+            get
+            {
+                return _currentTry <= _tryCount;
+            }
+        }
+
+        public static void TryExecute(Action action, IRequestRetryStrategy retryStrategy, CancellationToken cancellationToken)
+        {
+            do
+            {
+                try
+                {
+                    action();
+                    return;
+                }
+                catch (IOException e)
+                {
+                    retryStrategy.OnRequestFailure();
+
+                    if (!retryStrategy.ShouldRetry)
+                    {
+                        DebugLogger.LogError(string.Format("An IO Exception has occured: {0}. rethrowing.", e));
+                        throw;
+                    }
+
+                    DebugLogger.LogWarning(string.Format("An IO Exception has occured: {0}. retrying...", e));
+                    Threading.CancelableSleep(retryStrategy.DelayBeforeNextTry, cancellationToken);
+                }
+            } while (retryStrategy.ShouldRetry);
+        }
+
+        public static void TryExecute(Action action, CancellationToken cancellationToken)
+        {
+            TryExecute(action, new RetryStrategy(), cancellationToken);
+        }
+    }
+}
