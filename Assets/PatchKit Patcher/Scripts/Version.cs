9121b231dca08f3c341d3653bfead68e23a7c65c
Dzejkop <jakubtrad@gmail.com>
2018-07-09 14:30:16 +0200

    Implementation

--- Assets/PatchKit Patcher/Scripts/Version.cs
+++ Assets/PatchKit Patcher/Scripts/Version.cs
@@ -5,7 +5,12 @@ namespace PatchKit.Patching.Unity
         public const int Major = 4;
         public const int Minor = 0;
         public const int Patch = 0;
+
+#if PK_OFFICIAL
         public const string Suffix = "";
+#else
+        public const string Suffix = "-official";
+#endif
 
         public static string Value => $"v{Major}.{Minor}.{Patch}{Suffix}";
     }
