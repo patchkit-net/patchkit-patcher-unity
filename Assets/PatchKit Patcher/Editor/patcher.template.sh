461ca68836a156bb19ae723f9393ff948da7ae9f
Dzejkop <jakubtrad@gmail.com>
2018-09-03 13:44:55 +0200

    New Unix launch script now with argument parsing

--- Assets/PatchKit Patcher/Editor/patcher.template.sh
+++ Assets/PatchKit Patcher/Editor/patcher.template.sh
@@ -1,19 +1,51 @@
 #!/usr/bin/env sh
 
-EXEDIR=$1
-PATCHER_EXE=$2
-SECRET=$3
-INSTALLDIR=$4
-LOCKFILE=$5
+EXEDIR="."
+PATCHER_EXE="Patcher"
+LOCKFILE=""
+NETWORK_STATUS="online"
+
+while [ "$1" != "" ]; do
+    PARAM=`echo $1 | awk -F= '{print $1}'`
+    VALUE=`echo $1 | awk -F= '{print $2}'`
+    case $PARAM in
+        --exedir)
+            EXEDIR=$VALUE
+            ;;
+        --patcher-exe)
+            PATCHER_EXE=$VALUE
+            ;;
+        --secret)
+            SECRET=$VALUE
+            ;;
+        --installdir)
+            INSTALLDIR=$VALUE
+            ;;
+        --lockfile)
+            LOCKFILE=$VALUE
+            ;;
+        --network-status)
+            NETWORK_STATUS=$VALUE
+            ;;
+    esac
+    shift
+done
 
 LD_DIRS="`find "$EXEDIR" -name "x86_64" -printf "%p:"`"
 LD_DIRS="$LD_DIRS`find "$EXEDIR" -name "x86" -printf "%p:"`"
 
 export LD_LIBRARY_PATH="$LD_DIRS"
 
+ARGS="--installdir $INSTALLDIR --secret $SECRET"
+
 if [ -n "$LOCKFILE" ]
 then
-    "$EXEDIR/$PATCHER_EXE" --installdir "$INSTALLDIR" --secret "$SECRET" --lockfile "$LOCKFILE"
-else
-    "$EXEDIR/$PATCHER_EXE" --installdir "$INSTALLDIR" --secret "$SECRET"
+    ARGS="$ARGS --lockfile $LOCKFILE"
 fi
+
+if [ ! -z "$NETWORK_STATUS" ]
+then
+    ARGS="$ARGS --${NETWORK_STATUS}"
+fi
+
+"$EXEDIR/$PATCHER_EXE" $ARGS
