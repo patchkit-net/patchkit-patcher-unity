adebabf3d7ab5449e047c5ffdb8a83ee67dcde22
Dzejkop <jakubtrad@gmail.com>
2018-09-07 14:00:04 +0200

    Deleting torrent related code

--- Assets/Editor/Tests/RemoteResourceDownloaderTest.cs
+++ Assets/Editor/Tests/RemoteResourceDownloaderTest.cs
@@ -5,7 +5,6 @@ using NUnit.Framework;
 using PatchKit.Api.Models.Main;
 using PatchKit.Unity.Patcher.AppData.Remote;
 using PatchKit.Unity.Patcher.AppData.Remote.Downloaders;
-using PatchKit.Unity.Patcher.AppData.Remote.Downloaders.Torrents;
 using PatchKit.Unity.Patcher.Cancellation;
 
 public class RemoteResourceDownloaderTest
