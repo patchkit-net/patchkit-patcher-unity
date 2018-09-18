398d69043151c533a9c2098d80490fb21835266b
Dzejkop <jakubtrad@gmail.com>
2018-09-18 14:52:09 +0200

    Changed InvalidBuildTargets to ValidBuildTargets

--- Assets/PatchKit Patcher/Editor/Building/PostBuild.cs
+++ Assets/PatchKit Patcher/Editor/Building/PostBuild.cs
@@ -8,12 +8,18 @@ namespace PatchKit.Unity
 {
     public class PostBuild
     {
-        private static readonly BuildTarget[] InvalidBuildTargets = { BuildTarget.StandaloneOSXUniversal };
+        private static readonly BuildTarget[] ValidBuildTargets = {
+            BuildTarget.StandaloneLinux,
+            BuildTarget.StandaloneLinux64,
+            BuildTarget.StandaloneWindows,
+            BuildTarget.StandaloneWindows64,
+            BuildTarget.StandaloneOSXIntel64,
+        };
 
         [PostProcessBuild, UsedImplicitly]
         private static void Execute(BuildTarget buildTarget, string buildPath)
         {
-            if (InvalidBuildTargets.Contains(buildTarget))
+            if (!ValidBuildTargets.Contains(buildTarget))
             {
                 string archString = buildTarget.ToString();
                 EditorUtility.DisplayDialog("Warning", string.Format("PatchKit Patcher doesn't officially support {0} architecture. Errors may occur.", archString), "Ok");
