7b1091411d4954635599d9c81240278c96ff12c9
Tomasz Jaworski <tomasz.jaworski98@gmail.com>
2018-11-27 18:58:41 +0100

    Get rid of asset store libraries

--- Assets/Editor/InternalMenuItems.cs
+++ Assets/Editor/InternalMenuItems.cs
@@ -6,21 +6,6 @@ namespace PatchKit.Unity.Editor
 
 public class InternalMenuItems
 {
-    [MenuItem("Tools/PatchKit Patcher Internal/Upload Asset Store Package", false, 1)]
-    public static void UploadAssetStorePackage()
-    {
-        if (string.IsNullOrEmpty(InternalSettings.Email) || string.IsNullOrEmpty(InternalSettings.Password))
-        {
-            Debug.LogError("Please set email and password in internal settings");
-            return;
-        }
-
-        AssetStoreBatchMode.UploadAssetStorePackage(
-            InternalSettings.Email, InternalSettings.Password,
-            "PatchKit Patcher",
-            new [] {"PatchKit Patcher", "Plugins/PatchKit", "Plugins/UniRx", "StreamingAssets"});
-    }
-
     [MenuItem("Tools/PatchKit Patcher Internal/Settings", false, 100)]
     public static void Settings()
     {
