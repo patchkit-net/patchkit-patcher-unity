a38be12613210198b5e3e606a54b2c52a445fd83
Tomasz Jaworski <tomasz.jaworski98@gmail.com>
2019-03-07 13:28:47 +0100

    Update changelog and version

--- Assets/PatchKit Patcher/Scripts/Version.cs
+++ Assets/PatchKit Patcher/Scripts/Version.cs
@@ -3,7 +3,7 @@ namespace PatchKit.Unity.Patcher
     public static class Version
     {
         public const int Major = 3;
-        public const int Minor = 11;
+        public const int Minor = 12;
         public const int Release = 0;
 
         public static string Value
