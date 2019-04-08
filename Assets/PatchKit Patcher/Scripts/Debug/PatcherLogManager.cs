1f6b7e0539312b285f1912001f709fb88d5a0350
Tomasz Jaworski <tomasz.jaworski98@gmail.com>
2019-04-08 08:38:33 +0200

    WIP

--- Assets/PatchKit Patcher/Scripts/Debug/PatcherLogManager.cs
+++ Assets/PatchKit Patcher/Scripts/Debug/PatcherLogManager.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using System;
+using System.IO;
 using JetBrains.Annotations;
 using PatchKit.Logging;
 using PatchKit.Unity.Utilities;
@@ -10,112 +11,42 @@ namespace PatchKit.Unity.Patcher.Debug
 {
     public class PatcherLogManager : MonoBehaviour
     {
-        private static DefaultLogger _defaultLogger;
+        private StreamWriter _logWriter;
 
-        [NotNull]
-        public static DefaultLogger DefaultLogger
-        {
-            get { return LibPkAppsContainer.Resolve<DefaultLogger>(); }
-        }
+        private string _logPath;
+        private string _logGuid;
+        private string _logUrl;
 
-        private static PatcherLogManager _instance;
-        
-        [NotNull]
-        public static PatcherLogManager Instance
+        private void CreateLogFile()
         {
-            get
+            try
             {
-                UnityDispatcher.Invoke(() =>
+                _logPath = Path.GetTempFileName();
+
+                if (File.Exists(_logPath))
                 {
-                    if (_instance == null)
-                    {
-                        _instance = FindObjectOfType<PatcherLogManager>();
-                    }
-                }).WaitOne();
-                return _instance;
-            }
-        }
+                    File.Delete(_logPath);
+                }
 
-        public PatcherLogSentryRegistry SentryRegistry
-        {
-            get 
+                _logWriter = new StreamWriter(
+                    path: _logPath,
+                    append: false);
+            }
+            catch
             {
-                return _sentryRegistry;
+                // For safety ignore all exceptions
             }
         }
-
-        private DebugLogger DebugLogger;
-
-        private PatcherLogStream _stream;
-
-        private PatcherTemporaryLogFile _tempFile;
-
-        private PatcherLogRegisterTriggers _registerTriggers;
-
-        private PatcherLogStorage _storage;
-
-        public PatcherLogStorage Storage { get { return _storage; } }
-        
-        private PatcherLogSentryRegistry _sentryRegistry;
-
-        public bool IgnoreEditorErrors = true;
-
-        private bool _isEditor;
 
         private void Awake()
         {
-            DebugLogger = new DebugLogger(typeof(PatcherLogManager));
+            CreateLogFile();
 
-            DefaultLogger.Subscribe(new UnityMessageWriter(new SimpleMessageFormatter()));
+            _logGuid = Guid.NewGuid().ToString();
 
-            _isEditor = Application.isEditor;
-            _stream = new PatcherLogStream();
-            _tempFile = new PatcherTemporaryLogFile();
-            _registerTriggers = new PatcherLogRegisterTriggers();
-            _storage = new PatcherLogStorage();
-            _sentryRegistry = new PatcherLogSentryRegistry();
-
-            // Automatically call dispose on those objects when OnDestroy is called.
-            _stream.AddTo(this);
-            _tempFile.AddTo(this);
-            _registerTriggers.AddTo(this);
-
-            _stream.Messages.Subscribe(_tempFile.WriteLine).AddTo(this);
-
-            _registerTriggers.ExceptionTrigger.Subscribe(e =>
-                {
-                    if (_isEditor && IgnoreEditorErrors)
-                    {
-                        return;
-                    }
-                    
-                    _sentryRegistry.RegisterWithException(e, _storage.Guid.ToString());
-                })
-                .AddTo(this);
-
-            _registerTriggers.ExceptionTrigger.Throttle(TimeSpan.FromSeconds(5))
-                .Subscribe(e =>
-                {
-                    if (_isEditor && IgnoreEditorErrors)
-                    {
-                        return;
-                    }
-                    
-                    _tempFile.Flush();
-                    StartCoroutine(_storage.SendLogFileCoroutine(_tempFile.FilePath));
-                }).AddTo(this);
-        }
-
-        private void OnApplicationQuit()
-        {
-            if (!_storage.IsLogBeingSent)
-            {
-                return;
-            }
-
-            DebugLogger.Log("Cancelling application quit because log is being sent or is about to be sent.");
-            _storage.AbortSending();
-            Application.CancelQuit();
+            _logUrl = LibPatchKitApps.GetLogUrlPrediction(
+                guid: _logGuid,
+                appId: "patcher-unity");
         }
     }
 }
