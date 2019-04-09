e59e8e5d657be5e5671ac086aca65b99baa6864a
Tomasz Jaworski <tomasz.jaworski98@gmail.com>
2019-04-09 14:17:33 +0200

    Update version

--- Assets/PatchKit Patcher/Scripts/Version.cs
+++ Assets/PatchKit Patcher/Scripts/Version.cs
@@ -2,17 +2,16 @@ namespace PatchKit.Unity.Patcher
 {
     public static class Version
     {
-        public const int Major = 3;
-        public const int Minor = 12;
-        public const int Release = 1;
+        public const int Major = 4;
+        public const int Minor = 0;
+        public const int Release = 0;
 
-        public static string Value
-        {
 #if PK_OFFICIAL
-            get { return string.Format("v{0}.{1}.{2}-official", Major, Minor, Release); }
+        public const string Suffix = "-rc1.official";
 #else
-            get { return string.Format("v{0}.{1}.{2}", Major, Minor, Release); }
+        public const string Suffix = "-rc1";
 #endif
-        }
+
+        public static string Value => $"v{Major}.{Minor}.{Release}{Suffix}";
     }
 }
