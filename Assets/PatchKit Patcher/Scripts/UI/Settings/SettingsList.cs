53d378868a02867b97443eac89da0280399d6329
Jakub Szczyrk <jakubszczyrk@wp.pl>
2020-12-22 09:57:20 +0100

    Add settings analytics

---
+++ Assets/PatchKit Patcher/Scripts/UI/Settings/SettingsList.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using PatchKit.Unity.Patcher;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsList : MonoBehaviour
+{
+    public Toggle ToggleAnalytics;
+    private void Awake()
+    {
+        if (PlayerPrefs.GetInt("analytics") == 1)
+        {
+            PatcherStatistics.SetPermitStatistics(true);
+            ToggleAnalytics.isOn = true;
+        }
+    }
+
+    public bool SetAnalytics
+    {
+        set
+        {
+            PatcherStatistics.SetPermitStatistics(value);
+            PlayerPrefs.SetInt("analytics", value ? 1 : 0);
+            PlayerPrefs.Save();
+        }
+    }
+}
